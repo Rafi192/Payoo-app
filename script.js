ef439063d37876d5d6f227232d9dcd1673edcd74
Rafi192 <rafihasanmmsc@gmail.com>
2025-08-30 22:32:26 +0600

    Commit of payoo navbar

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const loginBtn = document.getElementById("btn-login")
 loginBtn.addEventListener('click', 
     function(event){
         event.preventDefault() // prevents the page getting refresh. dont need button type = button
-        console.log(loginBtn)
+        // console.log("Button clicked")
+        // console.log(event)
+        const mobileNumber = 1571422556;
+        const pinNumber  = 1234;
+        const mobileValue = document.getElementById('input-mobile')
+        const pinValue = document.getElementById('input-pin')
+
+        if(parseInt(mobileValue.value) === mobileNumber && parseInt(pinValue.value) === pinNumber){
+            window.location.href='./homepage.html'
+            
+        }
+        else{
+            console.log("Invalid Credentials")
+        }
+
 
 
 })
